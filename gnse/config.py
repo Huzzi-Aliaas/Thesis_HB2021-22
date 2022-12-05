4a476e8be215673716e6ac81f17a336556abe248
Huzzi-Aliaas <72714073+Huzzi-Aliaas@users.noreply.github.com>
2022-12-05 22:11:51 +0100

    Update config.py

--- gnse/config.py
+++ gnse/config.py
@@ -1,1 +1,11 @@
+"""
+Module containing helper routines, convenient abbreviations, and constants.
+"""
+import numpy as np
+import numpy.fft as nfft
 
+# -- CONVENIENT ABBREVIATIONS
+FTFREQ = nfft.fftfreq
+FT = nfft.ifft
+IFT = nfft.fft
+SHIFT = nfft.ifftshift
